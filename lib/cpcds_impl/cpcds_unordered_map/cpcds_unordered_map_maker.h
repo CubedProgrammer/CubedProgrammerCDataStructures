2b6afafbf230cfec765fceb1d6b136fdc5ba5473
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2020-02-11 23:11:01 -0500

    Adding functions to make empty unordered maps. Nothing has been tested yet, nor compiled.

--- lib/cpcds_impl/cpcds_unordered_map/cpcds_unordered_map_maker.h
+++ lib/cpcds_impl/cpcds_unordered_map/cpcds_unordered_map_maker.h
@@ -1,7 +1,8 @@
 #ifndef __cplusplus
 #ifndef INCLUDED_CPCDS_UNORDERED_MAP_MAKER_H
 #define INCLUDED_CPCDS_UNORDERED_MAP_MAKER_H
-#define define_cpcds_umk(name,__k__,__v__)struct cpcds_kvpair_##name{__k__ key;__v__ val;};typedef struct cpcds_kvpair_##name cpcds_kvpair_##name;cpcds_list_define(cpcds_kvpair_##name)struct cpcds_uom_##name{};
+#define __CPCDS_DEFAULT_UMR 1000
+#define define_cpcds_umk(name,__k__,__v__)struct cpcds_kvpair_##name{__k__ key;__v__ val;};typedef struct cpcds_kvpair_##name cpcds_kvpair_##name;cpcds_deque_define(cpcds_kvpair_##name)struct cpcds_um_##name{cpcds_deque_cpcds_kvpair_##name*ens;size_t room;};typedef struct cpcds_um_##name cpcds_um_##name;cpcds_um_##name cpcds_mk_um_room_##name(size_t rm){cpcds_um_##name um;um.room=rm;um.ens=(cpcds_deque_cpcds_kvpair_##name*)malloc(rm*sizeof(cpcds_deque_cpcds_kvpair_##name));return um;}cpcds_kvpair_##name cpcds_mk_um_empty_##name(){return cpcds_mk_um_room_##name(__CPCDS_DEFAULT_UMR);}
 #define cpcds_umk_define(__k__,__v__)define_cpcds_uom(__k__##__##__v__,__k__,__v__)
 #endif
 #endif
--- lib/cpcds_unordered_map.h
+++ lib/cpcds_unordered_map.h
@@ -5,7 +5,7 @@
 #include<stdlib.h>
 #include"cpcds_impl/cpcds_unordered_map/cpcds_unordered_map_maker.h"
 #include"cpcds_deque.h"
-#define define_cpcds_uom(name,__k__,__v__)define_cpcds_umk(name,__k__,__v__)
-#define cpcds_uom_define(__k__,__v__)define_cpcds_uom(__k__##__##__v__,__k__,__v__)
+#define define_cpcds_um(name,__k__,__v__)define_cpcds_umk(name,__k__,__v__)
+#define cpcds_um_define(__k__,__v__)define_cpcds_um(__k__##__##__v__,__k__,__v__)
 #endif
 #endif
