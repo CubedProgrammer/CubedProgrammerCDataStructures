3a75faa705e1d84de976ff4ea820297bad8ab68c
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2020-02-15 18:46:39 -0500

    Added iterator structure and functions for unordered map. Might not work depending on unordered map's contents.

--- lib/cpcds_unordered_map.h
+++ lib/cpcds_unordered_map.h
@@ -4,8 +4,9 @@
 #include<stdbool.h>
 #include<stdlib.h>
 #include"cpcds_impl/cpcds_unordered_map/cpcds_unordered_map_maker.h"
+#include"cpcds_impl/cpcds_unordered_map/cpcds_unordered_map_iter.h"
 #include"cpcds_deque.h"
-#define define_cpcds_um(name,__k__,__v__,efunc,hfunc)define_cpcds_umk(name,__k__,__v__,efunc,hfunc)typedef cpcds_um_##name*cpcds_um_##name##ptr;
+#define define_cpcds_um(name,__k__,__v__,efunc,hfunc)define_cpcds_umk(name,__k__,__v__,efunc,hfunc)define_cpcds_uiter(name,__k__,__v__)typedef cpcds_um_##name*cpcds_um_##name##ptr;
 #define cpcds_um_define(__k__,__v__,efunc,hfunc)define_cpcds_um(__k__##__##__v__,__k__,__v__,efunc,hfunc)
 #endif
 #endif
