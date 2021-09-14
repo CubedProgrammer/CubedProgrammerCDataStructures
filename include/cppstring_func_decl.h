0010105064a8847df851d0ee5dbf07508c4fb9b0
CubedProgrammer <47759847+CubedProgrammer@users.noreply.github.com>
2021-09-14 11:41:49 -0400

    Fixed incorrect include.

--- include/cppstring_func_decl.h
+++ include/cppstring_func_decl.h
@@ -1,7 +1,7 @@
 #ifndef __cplusplus
 #ifndef Included_cppstring_func_decl_h
 #define Included_cppstring_func_decl_h
-#include"cpcds_impl/cpcds_utils/hash_func.h"
+#include"cpcds_hash_func.h"
 #include"cppstring_maker.h"
 #include"cppstring_utils.h"
 #include"cppstring_extra.h"
